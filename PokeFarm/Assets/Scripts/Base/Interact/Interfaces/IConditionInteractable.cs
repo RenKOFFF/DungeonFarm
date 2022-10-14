241eef9546aadb19af73b9e5a1e96b9ba2e82994
LexKeivi <lex.keivi@mail.ru>
2022-10-14 17:28:06 +0500

    Update IConditionInterface
    Now IConditionInterface implement IInteractable

--- PokeFarm/Assets/Scripts/Base/Interact/Interfaces/IConditionInteractable.cs
+++ PokeFarm/Assets/Scripts/Base/Interact/Interfaces/IConditionInteractable.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine.Events;
 
-public interface IConditionInteractable
+public interface IConditionInteractable : IInteractable
 {
     public UnityEvent<IInteractable> OnConditionUpdatedEvent { get; }
     bool Condition { get; }
--- PokeFarm/Assets/Scripts/Base/Interact/Interfaces/IInteractable.cs
+++ PokeFarm/Assets/Scripts/Base/Interact/Interfaces/IInteractable.cs
@@ -1,5 +1,8 @@
+using UnityEngine;
+
 public interface IInteractable
 {
+    GameObject gameObject { get; }
     bool isCanInteract { get; }
     void Interact();
 }
