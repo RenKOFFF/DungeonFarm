57d1829b68f100fa2b9ef6313e073f3d8aace0f7
Alexey Koposov <cmango67@gmail.com>
2022-11-03 15:46:32 +0500

    Remove unused assets

--- PokeFarm/Assets/Scripts/Base/Managers/MarkerManager.cs
+++ PokeFarm/Assets/Scripts/Base/Managers/MarkerManager.cs
@@ -6,8 +6,15 @@ public class MarkerManager : MonoBehaviour
     [SerializeField] private Tilemap targetTilemap;
     [SerializeField] private TileBase markerTile;
 
+    public static MarkerManager Instance { get; private set; }
+
     public Vector3Int markedCellPosition;
     private Vector3Int oldCellPosition;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Update()
     {
