59c2de7f7f51ecf810d927174fe86c679ff5e75c
LexKeivi <lex.keivi@mail.ru>
2022-11-24 15:45:51 +0500

    Added auto load command from resources

--- PokeFarm/Assets/Scripts/Base/CommandStation/CommandStation.cs
+++ PokeFarm/Assets/Scripts/Base/CommandStation/CommandStation.cs
@@ -1,16 +1,44 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class CommandStation : MonoBehaviour, IInteractable
 {
     public bool isCanInteract => true;
 
+    private MonstersInteractionWayDataSO[] _allMonstersInteractionWays;
     public MonstersInteractionWay CurrentCommand { get;  private set; }
+    [SerializeField] private GameObject _usingInterface;
+    [SerializeField] private CommandButton _commandButtonPrefab;
 
-    public UnityEvent<MonstersInteractionWay> OnCommandSelectedEvent = new UnityEvent<MonstersInteractionWay>();
+    [HideInInspector] public UnityEvent<MonstersInteractionWay> OnCommandSelectedEvent = new UnityEvent<MonstersInteractionWay>();
+
+    private void Awake()
+    {
+        Init();
+        _usingInterface?.SetActive(false);
+    }
+
+    private void Init()
+    {
+        _allMonstersInteractionWays = Resources.LoadAll<MonstersInteractionWayDataSO>("Monsters/InteractionsWays");
+
+        var commandList = _usingInterface.GetComponentInChildren<GridLayoutGroup>();
+
+        foreach (var command in _allMonstersInteractionWays)
+        {
+            var button = _commandButtonPrefab;
+            button.InteractData = command;
+            button.InteractionWay = command.InteractionWay;
+            button.RefreshButtonData();
+
+            Instantiate(button, commandList.transform);
+        }
+    }
 
     public void Interact()
     {
@@ -19,7 +47,7 @@ public class CommandStation : MonoBehaviour, IInteractable
 
     private void ShowInterface()
     {
-        throw new NotImplementedException();
+        _usingInterface?.SetActive(true);
     }
 
     private void OnEnable()
@@ -30,5 +58,10 @@ public class CommandStation : MonoBehaviour, IInteractable
     private void ChangeCurrentCommand(MonstersInteractionWay selectedCommand)
     {
         CurrentCommand = selectedCommand;
+    }
+
+    private void OnDisable()
+    {
+        OnCommandSelectedEvent.RemoveListener(ChangeCurrentCommand);
     }
 }
