3a532f00e3527bb0f0ffd5633a3c56b1e1408df0
Alexey Koposov <cmango67@gmail.com>
2022-12-01 12:22:55 +0500

    Add PlantingCycleTile

---
+++ PokeFarm/Assets/Scripts/Base/Tiles/PlantingCycleTile.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Data/PlantingCycleTile")]
+public class PlantingCycleTile : ScriptableObject
+{
+    public TileBase tile;
+
+    [CanBeNull] public TileBase previousCycleTile;
+    [CanBeNull] public TileBase nextCycleTile;
+
+    [CanBeNull] public Item interactsWithTool;
+}
