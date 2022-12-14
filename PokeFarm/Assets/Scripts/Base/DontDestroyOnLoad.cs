c3d12e5e51f8ebdd18de780bc7b95dd8c28b5ecf
re_nKOFF <lex.keivi@mail.ru>
2022-12-14 14:12:24 +0500

    Dont Destroy on load

---
+++ PokeFarm/Assets/Scripts/Base/DontDestroyOnLoad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Base
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        public static DontDestroyOnLoad Instance { get; private set; }
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(Instance);
+                return;
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+}
