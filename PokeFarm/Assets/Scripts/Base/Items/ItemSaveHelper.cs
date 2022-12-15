4d266ee231587eb48a7cfbbec9bb60477f67e9a8
Alexey Koposov <cmango67@gmail.com>
2022-12-15 11:48:02 +0500

    Improve safety of containers loading

--- PokeFarm/Assets/Scripts/Base/Items/ItemSaveHelper.cs
+++ PokeFarm/Assets/Scripts/Base/Items/ItemSaveHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
+using UnityEngine;
 
 namespace Base.Items
 {
@@ -32,8 +33,18 @@ namespace Base.Items
             => new(itemSlot.item?.Name, itemSlot.amount);
 
         private static ItemSlot SlotSaveItemToItemSlot(SlotSaveItem slotSaveItem)
-            => slotSaveItem.ItemName == null
-                ? new ItemSlot()
-                : new ItemSlot(GameDataController.AllItems[slotSaveItem.ItemName], slotSaveItem.Amount);
+        {
+            if (slotSaveItem.ItemName == null)
+                return new ItemSlot();
+
+            if (!GameDataController.AllItems.ContainsKey(slotSaveItem.ItemName))
+            {
+                Debug.LogError("Ошибка загрузки предмета." +
+                               $" Не удалось найти в игровых файлах предмет с именем [{slotSaveItem.ItemName}].");
+                return new ItemSlot();
+            }
+
+            return new ItemSlot(GameDataController.AllItems[slotSaveItem.ItemName], slotSaveItem.Amount);
+        }
     }
 }
