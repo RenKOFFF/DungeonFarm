37d4aa3a839aa98da48ced7447f548c9f450208a
re_nKOFF <lex.keivi@mail.ru>
2022-12-21 22:33:02 +0500

    Scene transitions fix

--- PokeFarm/Assets/Scripts/Objects/MonstersCombatTeleportDoor.cs
+++ PokeFarm/Assets/Scripts/Objects/MonstersCombatTeleportDoor.cs
@@ -6,9 +6,7 @@ using UnityEngine;
 
 public class MonstersCombatTeleportDoor : TeleportDoor
 {
-    private bool _isSwitchingSceneNow;
     private SelectCombatMonsters _selectCombatMonsters;
-
 
     private void Start()
     {
@@ -22,31 +20,8 @@ public class MonstersCombatTeleportDoor : TeleportDoor
 
     public void GoToCombat()
     {
-        if (_isSwitchingSceneNow) return;
+        if (isSwitchingSceneNow) return;
         
         StartCoroutine(WaitSwitchScene());
-    }
-    
-    private IEnumerator WaitSwitchScene()
-    {
-        _isSwitchingSceneNow = true;
-        
-        var waitFading = true;
-        Fader.Instance.FadeIn(() => waitFading = false);
-        while (waitFading)
-        {
-            yield return null;
-        }
-        
-        Teleport();
-        
-        waitFading = true;
-        Fader.Instance.FadeOut(() => waitFading = false);
-        while (waitFading)
-        {
-            yield return null;
-        }
-        
-        _isSwitchingSceneNow = false;
     }
 }
