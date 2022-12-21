5bd54b176a95463ed9be83c4f21418e603c3f605
Alexey Koposov <cmango67@gmail.com>
2022-12-21 21:01:34 +0500

    Add monster attack animation

--- PokeFarm/Assets/Scripts/Base/Autobattle/AutobattleMonster.cs
+++ PokeFarm/Assets/Scripts/Base/Autobattle/AutobattleMonster.cs
@@ -9,9 +9,18 @@ public class AutobattleMonster : MonoBehaviour
 
     [field: SerializeField] public float TimeToMakeMoveInSeconds { get; set; } = 1;
     [field: SerializeField] public float TimeLeftToMakeMoveInSeconds { get; set; } = 1;
+    [field: SerializeField] public float AttackTimeInSeconds { get; set; } = 0.3f;
 
     public bool IsDead => Health == 0;
     public bool IsReadyToAttack => TimeLeftToMakeMoveInSeconds <= 0;
+
+    private bool NeedPlayAttackAnimation { get; set; }
+    private bool NeedPlayReturnFromAttackAnimation { get; set; }
+
+    private float MinimalDistanceToEndAnimation { get; set; } = 0.01f;
+    private Vector3 StartAnimationPosition { get; set; }
+    private AutobattleMonster EnemyMonster { get; set; }
+    private float AnimationSpeed { get; set; }
 
     private MonsterStats Stats { get; set; }
     private float Health { get; set; }
@@ -36,8 +45,21 @@ public class AutobattleMonster : MonoBehaviour
 
     public void Attack(AutobattleMonster enemy)
     {
+        PlayAttackAnimation(enemy);
+
         enemy.GetDamage(Stats.Strength);
         TimeLeftToMakeMoveInSeconds = TimeToMakeMoveInSeconds;
+    }
+
+    private void PlayAttackAnimation(AutobattleMonster autobattleMonster)
+    {
+        var endAnimationPosition = autobattleMonster.transform.position;
+
+        EnemyMonster = autobattleMonster;
+        StartAnimationPosition = transform.position;
+        AnimationSpeed = Vector3.Distance(StartAnimationPosition, endAnimationPosition) / AttackTimeInSeconds * 2;
+
+        NeedPlayAttackAnimation = true;
     }
 
     private void GetDamage(float enemyStrength)
@@ -63,5 +85,41 @@ public class AutobattleMonster : MonoBehaviour
     public void Die()
     {
         GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+
+    private void Update()
+    {
+        if (NeedPlayAttackAnimation)
+        {
+            var endAnimationPosition = EnemyMonster.transform.position;
+
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                endAnimationPosition,
+                AnimationSpeed * Time.deltaTime);
+
+            var distanceToEndAnimation = Vector3.Distance(transform.position, endAnimationPosition);
+
+            if (distanceToEndAnimation < MinimalDistanceToEndAnimation)
+            {
+                NeedPlayAttackAnimation = false;
+                NeedPlayReturnFromAttackAnimation = true;
+            }
+
+            return;
+        }
+
+        if (NeedPlayReturnFromAttackAnimation)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                StartAnimationPosition,
+                AnimationSpeed * Time.deltaTime);
+
+            var distanceToEndAnimation = Vector3.Distance(transform.position, StartAnimationPosition);
+
+            if (distanceToEndAnimation < MinimalDistanceToEndAnimation)
+                NeedPlayReturnFromAttackAnimation = false;
+        }
     }
 }
