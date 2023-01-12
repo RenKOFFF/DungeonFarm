6e29da29cc57c2a6466a12bba22747ba549ed4d4
Alexey Koposov <cmango67@gmail.com>
2023-01-12 17:36:22 +0500

    Disable autobattle start button when no monsters selected

---
+++ PokeFarm/Assets/Scripts/Base/Autobattle/AutobattleStartButton.cs
@@ -0,0 +1,21 @@
+﻿using Base.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Base.Autobattle
+{
+    public class AutobattleStartButton : MonoBehaviour
+    {
+        private Button ButtonComponent { get; set; }
+
+        private void Awake()
+        {
+            ButtonComponent = GetComponent<Button>();
+        }
+
+        private void Update()
+        {
+            ButtonComponent.interactable = MonstersManager.SelectedCombatMonsters.Count > 0;
+        }
+    }
+}
